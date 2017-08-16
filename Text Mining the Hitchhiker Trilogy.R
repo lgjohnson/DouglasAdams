356deee188ad0f925d6e7db6d722f3203d19a5c6
Lee Johnson <lgjohnson@Lees-MacBook-Pro.local>
2017-08-15 21:08:08 -0700

    used purl to create R file

---
+++ Text Mining the Hitchhiker Trilogy.R
@@ -0,0 +1,11 @@
+## ----installLibraries, include = FALSE-----------------------------------
+require("tidytext")
+
+## ----readData------------------------------------------------------------
+#setwd("~/Documents/Analytics/R/DouglasAdams")
+book1 <- readLines("data/HitchHiker.txt")
+head(book1,20)
+
+## ------------------------------------------------------------------------
+3
+
